c46565ea2334ee0e34dd5076a5b17c8e915166e1
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-01-04 18:47:09 -0600

    Read Description
    Petition to rename SketchHead to "Become a Drawing Done by a 40 Year Old Man Living in His Mother's Basement, With No Money, No Father, No Life, No Brain, Anxiety, Depression, and Emotional Damage"

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,15 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    [Header("Object that requires mental help for requirement of mental help because we need mental help and it's mental help we need mental help")]
+    public GameObject paltfrom;
+    float pso = 0;
+    [Header("game over scontafiksmiutigpdjphoh8euo1d")]
+    public GameObject gmaeOevrCavnas;
     // Start is called before the first frame update
     void Start()
     {
-
+        for (int i = 0; i < 1000; i++)
+        {
+            SpawnPlatforms();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
 
+    void SpawnPlatforms()
+    {
+        Instantiate(paltfrom, new Vector3(Random.value * 10 - 5f, pso, 0.5f), Quaternion.identity);
+        pso += 2.5f;
+    }
+
+    public void GmaeOevr()
+    {
+        gmaeOevrCavnas.SetActive(true);
     }
 }
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,15 +5,52 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("ronald mcdonald eating taco bell")]
+    public Rigidbody2D rb;
+    [Header("default downward fatherless man")]
+    public float downwardSpiralForHavingAnxietyDepressionAndEmotionalDamage = 20f;
+    [Header("default speed for directionals movenejtwnad")]
+    public float movementSpeedForBecomingA40YearOldLivingInTheirMothersBasementWithAnxietyDepressionAndEmotionalDamage = 10f;
+    [Header("default direction speed for becoming a roblox player. your entire brain will be roblox, 4 + 4 will equal adopt me, closet = hide from rush. your brain is no more and you have fallen victim to anxiety, emotional damage depression, no money, no life, no father")]
+    public float directionSpeedForBeingARobloxPlayerWithAnxietyDepressionEmotionalDamageNoFatherNoBrainsNoMoneyAndNoLife = 0f;
+    [Header("how much time you've spent as a fatherless, brainless, broke, goofy ah no-life living in their mother's basement")]
+    public Text howMuchTimeYouHaveSpentAsAFatherlessBrainlessBrokeGoofyAhNoLifeLivingInTheirMothersBasement;
+    public float maxAmountOfTimeYouHaveSpentAsAFatherlessBrainlessBrokeGoofyAhNoLifeLivingInTheirMothersBasement;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // e e e e e e  e e e  e e e e  e e e e e e e  eeeeeee  ee e e e e e e e e e e ee e e e e e e e ee xncbvhubaeridocxjvhbgadsixcvhjbegrijsodfkjm bgrjdfs809ikjcvn kdfsu9ikngixcvm bnifshndmv,nb xdfjmjodfsikjxn cdfsixkjncm sdfipokmcvx, mfasd,zokj cxvlb;sdgzfxpcvcn.ds:Ocxadfb,hjxcdsibhjdhyu9 gcgfcgf cgf
+        directionSpeedForBeingARobloxPlayerWithAnxietyDepressionEmotionalDamageNoFatherNoBrainsNoMoneyAndNoLife = Input.GetAxis("Horizontal") * movementSpeedForBecomingA40YearOldLivingInTheirMothersBasementWithAnxietyDepressionAndEmotionalDamage;
+        // /j
+        if (directionSpeedForBeingARobloxPlayerWithAnxietyDepressionEmotionalDamageNoFatherNoBrainsNoMoneyAndNoLife < 0)
+        {
+            this.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (rb.velocity.y > 0 && transform.position.y > maxAmountOfTimeYouHaveSpentAsAFatherlessBrainlessBrokeGoofyAhNoLifeLivingInTheirMothersBasement)
+        {
+            maxAmountOfTimeYouHaveSpentAsAFatherlessBrainlessBrokeGoofyAhNoLifeLivingInTheirMothersBasement = transform.position.y;
+        }
+        howMuchTimeYouHaveSpentAsAFatherlessBrainlessBrokeGoofyAhNoLifeLivingInTheirMothersBasement.text = "Score: " + Mathf.Round(maxAmountOfTimeYouHaveSpentAsAFatherlessBrainlessBrokeGoofyAhNoLifeLivingInTheirMothersBasement).ToString();
+    }
 
+    void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = directionSpeedForBeingARobloxPlayerWithAnxietyDepressionEmotionalDamageNoFatherNoBrainsNoMoneyAndNoLife;
+        rb.velocity = velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.velocity = new Vector3(rb.velocity.x, downwardSpiralForHavingAnxietyDepressionAndEmotionalDamage, 0);
     }
 }
